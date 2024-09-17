dc8245262f3067a2257025fc29b85b1100cf284b
prize15 <abhinavaseeja2000@gmail.com>
2024-09-17 11:07:11 +0530

    Done with ES7

---
+++ ES7/ES7.java
@@ -0,0 +1,12 @@
+// includes
+
+const arr = [1, 2, 3, 4];
+console.log(arr.includes(3)); // true
+console.log(arr.includes(5)); // false
+
+//---------------------------------------------------------------
+
+//exponential operator
+
+console.log(2 ** 3); // 8
+console.log(5 ** 2); // 25

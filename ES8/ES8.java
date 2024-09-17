50cd8665166b1bf39fca95acb1a665bd4b4ceaa9
prize15 <abhinavaseeja2000@gmail.com>
2024-09-17 11:08:53 +0530

    Done with ES8

---
+++ ES8/ES8.java
@@ -0,0 +1,22 @@
+//Object.entries
+
+const obj = { a: 1, b: 2, c: 3 };
+console.log(Object.entries(obj)); 
+// [ ['a', 1], ['b', 2], ['c', 3] ]
+
+//----------------------------------------------------
+
+//Object.values
+
+const obj1 = { a: 1, b: 2, c: 3 };
+console.log(Object.values(obj1)); 
+// [1, 2, 3]
+
+//-----------------------------------------------------
+
+// String padding 
+
+console.log('5'.padStart(3, '0')); // '005'
+console.log('5'.padEnd(3, '0'));   // '500'
+
+//---------------------------------------------------------
